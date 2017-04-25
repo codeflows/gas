ae90f9add60f3e2d3e37c834d209ebc9770e88d8
Jari Aarniala <jari.aarniala@reaktor.com>
2017-04-25 09:21:37 +0300

    Scrape listing titles

--- gas.go
+++ gas.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"regexp"
 
-func main() {
-	fmt.Printf("Hello, world.\n")
+	"github.com/PuerkitoBio/goquery"
+)
+
+func ScrapeMuusikoidenNet() {
+	doc, err := goquery.NewDocument("https://muusikoiden.net/tori/?category=0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	idFromURL, _ := regexp.Compile("/(\\d+)$")
+
+	doc.Find("td.tori_title").Each(func(i int, title *goquery.Selection) {
+		// TODO "Myydään" ääkköset are mangled
+		category := title.Find("b").Text()
+		link := title.Find("a")
+		name := link.Text()
+		url, _ := link.Attr("href")
+		id := idFromURL.FindStringSubmatch(url)[1]
+		fmt.Printf("%s %s %s %s\n", category, name, url, id)
+	})
 }
 
+func main() {
+	ScrapeMuusikoidenNet()
+}
