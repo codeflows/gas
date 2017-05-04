3e4fe8e00fab8bc31c0f310b961ea7edbdd29d03
Jari Aarniala <jari.aarniala@gmail.com>
2017-05-04 21:18:46 +0300

    Parse more stuff

--- gas.go
+++ gas.go
@@ -9,11 +9,15 @@ import (
 )
 
 type item struct {
-	id       string
-	category string
-	title    string
-	url      string
+	id          string
+	category    string
+	title       string
+	url         string
+	description string
+	price       string
 }
+
+var idFromURL, _ = regexp.Compile("/(\\d+)$")
 
 func ScrapeMuusikoidenNet() {
 	doc, err := goquery.NewDocument("https://muusikoiden.net/tori/?category=0")
@@ -21,24 +25,28 @@ func ScrapeMuusikoidenNet() {
 		log.Fatal(err)
 	}
 
-	idFromURL, _ := regexp.Compile("/(\\d+)$")
-
 	elements := doc.Find("td.tori_title")
 
 	items := make([]item, elements.Length())
 
 	elements.Each(func(i int, titleContainer *goquery.Selection) {
-		// TODO "Myydään" ääkköset are mangled
 		category := titleContainer.Find("b").Text()
 		link := titleContainer.Find("a")
 		title := link.Text()
 		url, _ := link.Attr("href")
 		id := idFromURL.FindStringSubmatch(url)[1]
-		item := item{id, category, title, url}
+
+		descriptionContainer := titleContainer.Parent().Siblings().Find("font.msg")
+		description := descriptionContainer.Text()
+		price := descriptionContainer.SiblingsFiltered("p").Text()
+
+		item := item{id, category, title, url, description, price}
 		items[i] = item
 	})
 
-	fmt.Printf("%s\n", items)
+	for _, item := range items {
+		fmt.Printf("%s %s\n", item.title, item.price)
+	}
 }
 
 func main() {
