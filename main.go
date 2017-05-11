4c5840d60e87bf3561a55700ac541b2333157a65
Jari Aarniala <jari.aarniala@reaktor.com>
2017-05-11 09:32:10 +0300

    Learn filtering

--- main.go
+++ main.go
@@ -1,10 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func filter(vs []Ad, f func(Ad) bool) []Ad {
+	vsf := make([]Ad, 0)
+	for _, v := range vs {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
+	return vsf
+}
 
 func main() {
+	lastSeenAdID, err := ioutil.ReadFile("last_seen")
+	if err != nil {
+		println("No last_seen state")
+	}
+	fmt.Printf("Last seen is %s\n", string(lastSeenAdID))
+
 	ads := ScrapeAds()
+	newAds := filter(ads, func(ad Ad) bool {
+		return true
+	})
+
+	// Ads seem to be ordered in non-id order? Probably publish date.
+	fmt.Printf("%d new ads since last run\n", len(newAds))
+
 	for _, item := range ads {
-		fmt.Printf("%s %s\n", item.title, item.price)
+		fmt.Printf("%s %s %s\n", item.id, item.title, item.price)
 	}
 }
