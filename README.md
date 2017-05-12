7be14508b475f5372d24845536bdbdd043851539
Jari Aarniala <jari.aarniala@gmail.com>
2017-05-13 00:15:02 +0300

    Cleanup

--- README.md
+++ README.md
@@ -1,20 +1,6 @@
 # gas
 
 ```bash
-$ go run *.go
-Gibson J-45 ViINTAGE 2016 Hinta: 2450 €
-Camco pedaali Hinta: 50 €
-Proco Rat original, Tämän viikon tarjous! Hinta: 160 €
-Vintage Celestion G12M Black Back 12", Originaali,TARJOUS! Hinta: 150 €
-Paiste Formula 602 Hinta: 200 €
-Fender Jaguar -66 Hinta: 2600 €
-Gator pedaalilauta  GBT-PRO Hinta: 100 €
-Behringer BVT4500H nuppi + Ashdown 250w kaappi Hinta: 280 €
-Jackson pro soloist SL2Q HT Hinta: 850 €
-Tokai AJB-28 Hinta: 340 €
-Celestion Alnico Blue 12"/ 8ohm -> WGS Blackhawk 12"/8ohm
-ENGL Powerball 100w Hinta: 680 €
-Earthquaker Devices Acapulco Gold
-Martin lx1e
-Vester Hinta: 35 €
+$ SLACK_WEBHOOK_URL="https://hooks.slack.com/..." go run *.go
+Posting 15 new ads to Slack
 ```
--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-)
+import "fmt"
 
 func filter(vs []Ad, f func(Ad) bool) []Ad {
 	vsf := make([]Ad, 0)
@@ -16,22 +13,16 @@ func filter(vs []Ad, f func(Ad) bool) []Ad {
 }
 
 func main() {
-	lastSeenAdID, err := ioutil.ReadFile("last_seen")
-	if err != nil {
-		println("No last_seen state")
-	}
-	fmt.Printf("Last seen is %s\n", string(lastSeenAdID))
-
 	ads := ScrapeAds()
 	newAds := filter(ads, func(ad Ad) bool {
 		return true
 	})
 
+	// TODO Actually only send new ads
 	// Ads seem to be ordered in non-id order? Probably publish date.
-	fmt.Printf("%d new ads since last run\n", len(newAds))
+	fmt.Printf("Posting %d new ads to Slack\n", len(newAds))
 
 	for _, ad := range ads {
-		fmt.Printf("%s %s %s\n", ad.id, ad.title, ad.price)
 		slackError := SendAdToSlack(ad)
 		if slackError != nil {
 			fmt.Printf("Sending to Slack failed: %s\n", slackError)
