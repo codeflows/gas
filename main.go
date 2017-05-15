03d79980a2edd649776b9aee88c4c5fd5aecc20f
Jari Aarniala <jari.aarniala@gmail.com>
2017-05-15 21:27:42 +0300

    Read ad ids from a JSON file

--- main.go
+++ main.go
@@ -3,16 +3,22 @@ package main
 import "fmt"
 
 func main() {
+	seenAdIds, readError := ReadSeenAdIds()
+	if readError == nil {
+		fmt.Printf("Read a list of %d already seen ad ids\n", len(seenAdIds))
+	} else {
+		fmt.Printf("Couldn't read already seen ad ids, falling back to an empty list. Error: %s\n", readError)
+		seenAdIds = make(SeenAds)
+	}
+
 	ads := ScrapeAds()
 	fmt.Printf("Read %d ads from Muusikoiden.net\n", len(ads))
-
-	seenAdIds := ReadSeenAdIds()
 
 	newAds := Filter(ads, func(ad Ad) bool {
 		alreadySeen := seenAdIds[ad.id]
 		return !alreadySeen
 	})
-	fmt.Printf("Posting %d new ads to Slack\n", len(newAds))
+	fmt.Printf("%d ads are new\n", len(newAds))
 
 	return
 
--- state.go
+++ state.go
@@ -1,8 +1,33 @@
 package main
 
-// ReadSeenAdIds returns a list of ad ids we've already seen.
-// Modified ads apparently resurface in the list,
-// maybe we could also store a hash of the content?
-func ReadSeenAdIds() map[string]bool {
-	return make(map[string]bool)
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+// SeenAds is the type used to represent seen ad ids
+type SeenAds map[string]bool
+
+const filename = "./seen_ad_ids"
+
+// ReadSeenAdIds returns a map of ad ids we've already seen.
+// Not an array since Go doesn't have `contains` for arrays out the box 😬 😎
+func ReadSeenAdIds() (SeenAds, error) {
+	file, fileError := ioutil.ReadFile(filename)
+
+	if fileError != nil {
+		return nil, fileError
+	}
+
+	var ids SeenAds
+	jsonError := json.Unmarshal(file, &ids)
+
+	if jsonError != nil {
+		return nil, jsonError
+	}
+	return ids, nil
+}
+
+func WriteSeenAdIds(seenAds SeenAds) error {
+	return nil
 }
